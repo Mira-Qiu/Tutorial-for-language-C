88d0ab85c3e31e8aaee714e475834701e538c63f
Mira Qiu <qmm0607@gmail.com>
2018-11-05 23:58:48 -0500

    Update 3Arrays.c

--- 3Arrays.c
+++ 3Arrays.c
@@ -11,7 +11,7 @@ void display(int x[])
 
 int main()
 {
-  int *a;
+  int *a;  //The location  of a is in the stack, but the location of array are in heap.
   int i;
   a = (int*)malloc(5*sizeof(int));
   //allocate space in heap for the array;
@@ -20,3 +20,11 @@ int main()
   display(a);
   return 0;
  }
+
+
+/* The declaration int* a inform the C compilar that the content of the variable a can point to an integer's location.
+*  a = (int*)malloc(5*sizeof(int)); does two things:
+* 1.build-in function "malloc" which stands for memory allocation, is defined in the header file "stdlib.h", This function allocaties space in the heap.
+* 2. The function returns the address of the first of the five consecutive locations that are in heap.
+* The address would be the content of a.
+*/
