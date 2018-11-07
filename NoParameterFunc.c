c9aca56b0e72f5a5eab10bd9746238a4c949361b
Mira Qiu <qmm0607@gmail.com>
2018-11-07 00:10:53 -0500

    Update NoParameterFunc.c

--- NoParameterFunc.c
+++ NoParameterFunc.c
@@ -1,1 +1,22 @@
-#
+#include<stdio.h>
+
+void func(void (*f)());
+void print()
+{
+   pritf("Hello world\n");
+}
+void func(void (*f)())
+{
+  (*f)();
+}
+int main()
+{
+  func(print);
+  return 0;
+}
+
+
+/*
+*  When we pass f to func we pass it as a pointer. This means the pointer points the beginning of the body of f;
+*  When we call fun we should dereferences for f. That is "*f" is passed;
+*/
