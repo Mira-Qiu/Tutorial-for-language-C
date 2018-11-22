6000bf5ab4d1b8b6fd973ad591460b4365e1b15b
Mira Qiu <qmm0607@gmail.com>
2018-11-22 12:40:21 -0500

    Update 2Pointer.c

--- 2Pointer.c
+++ 2Pointer.c
@@ -7,7 +7,7 @@ int main(>
   
   a = -7;
   x = &a;
-  *x = *x + 2;
+  *x = *x + 2;    //Mark 1;
   printf("The content of variable a is: %d\n",a);
   printf("The content of variable a is: %d\n", *x);
 }
@@ -15,3 +15,9 @@ int main(>
 * The content of variable a is: -5
 * The content of variable a is: -5
 */
+
+// Location X points to variable a. 
+// Mark 3, right hand side gets executed. Execution of *x means : start from location x and derefere to the location whose address is in x.
+// (which is the address of a) and pick the value of that location ( which is -1).从地址X开始，断开在X里面的地址的关联，取出那个地址里面的数值。然后计算。
+//现在相加，地址里的value 就是-5。we have *x = -5.To system this  means:it starts from location x and dereference to the location whose address is in x
+// and insert 5 in that location.从地址x开始，与地址x断关联，插入-5到这个地址。
