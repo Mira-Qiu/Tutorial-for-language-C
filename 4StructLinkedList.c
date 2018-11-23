ceea025cb9c89376f9d1b19cafef075008262325
Mira Qiu <qmm0607@gmail.com>
2018-11-23 00:07:21 -0500

    Update 4StructLinkedList.c

--- 4StructLinkedList.c
+++ 4StructLinkedList.c
@@ -14,10 +14,10 @@ int main()
   head = NULL;
   for(i = 7; i > =1 ; i = i - 2)
   {
-    newNode =(Rec*)malloc(sizeof(Rec)); //Right hand: allocates space with the size of the struct and returns its address.
-    newNode ->key = i; // dereferences, access the location of the key and inserts the value into this location
-    newNode ->next = head; //dereferences, access the location of the variables next and inserts the value;
-    head = newNode; //Inserts the content of newNode to the head location. newNode and head point to the same location
+    newNode =(Rec*)malloc(sizeof(Rec)); 
+    newNode ->key = i; 
+    newNode ->next = head;
+    head = newNode; 
    }
    
    temp = head;
