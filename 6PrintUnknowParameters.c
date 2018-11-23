f8328a33f6c51a2eb0bcc93298a14c8aaac45216
Mira Qiu <qmm0607@gmail.com>
2018-11-23 15:45:51 -0500

    Update 6PrintUnknowParameters.c

--- 6PrintUnknowParameters.c
+++ 6PrintUnknowParameters.c
@@ -20,3 +20,25 @@ int main() {
 	return 0;
 }
 
+//Below is the example which had been mentioned at README file.
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void print(int arg, ...){
+  va_list ap;
+  int i;
+  va_start(ap, arg); 
+  for (i = arg; i >= 0; i = va_arg(ap, int))
+    printf("%d ", i);
+   va_end(ap);
+   printf("\n");
+}
+
+int main(void){
+  print(5, 2, 14, 84, 97, 15, 24, 48, -1);
+  print(84, 51, -1);
+  print(-1);
+  print(1, -1);
+  return 0;
+}
